12c0a09a80abc2233db742a7913ce4ab998707d7
Chaitra <chaitramp9@gmail.com>
2019-05-26 18:12:53 +0530

    first commit

---
+++ src/test/java/com/qsp/OrangeHRMUtils/Utility.java
@@ -0,0 +1,15 @@
+package com.qsp.OrangeHRMUtils;
+
+
+import java.util.Date;
+
+public class Utility {
+	
+	public static String getDate() {
+		Date d = new Date();
+		String date =  d.toString().replace(" ", "_").replace(":", "_");
+		System.out.println(date);
+		return date;
+	}
+
+}
